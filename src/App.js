2293d670b38b5337c94d163e4fbadd069926e9bf
Sajid Zaman <sajid-619@users.noreply.github.com>
2020-03-12 23:02:49 +0600

    React App Demo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
 import React from "react";
+import logo from "./React.js_logo-512.png";
+import "./styles.css";
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="App-title">React App</h1>;
+        <div className="container">
+          Add an Item...
+          <br />
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Write a todo"
+          />
+          <button className="btn">Submit</button>
+          <div className="list">
+            <ul>
+              <li>
+                <input type="checkbox" name="" id="" />
+                Record youtube video
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img src={logo} width="100" height="100" className="logo" alt="" />;
+      </div>
+    );
+  }
+}
 export default App;
